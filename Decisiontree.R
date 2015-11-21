d536c9363eb0a680d6e279b2f04a8f73721b3696
Jaini Vora <voraj@iu.edu>
2015-11-21 12:07:19 -0500

    initial

---
+++ Decisiontree.R
@@ -0,0 +1,6 @@
+setwd("C:/Users/Jaini/Desktop/OnlineNewsPopularity")
+Dummy.data <- read.delim("C:/Users/Jaini/Desktop/OnlineNewsPopularity/Dummy data.txt")
+str(Dummy.data)
+library(rpart)
+set.seed(245)
+flags=sample(2,nrow(Dummy.data), replace = TRUE , prob = c(0.7,0.3))
