fc5bbd619a51678b6e60c084e9fbd6e301d9d010
Jaini Vora <voraj@iu.edu>
2015-11-21 14:54:28 -0500

    new changes
    Changes

--- Decisiontree.R
+++ Decisiontree.R
@@ -4,3 +4,8 @@ str(Dummy.data)
 library(rpart)
 set.seed(245)
 flags=sample(2,nrow(Dummy.data), replace = TRUE , prob = c(0.7,0.3))
+trainset=Dummy.data[which(flags==1),]
+testset=Dummy.data[which(flags==2),]
+dtree= rpart(shares~.,data=trainset, control=rpart.control(10))
+plot(dtree)
+dtree$variable.importance
