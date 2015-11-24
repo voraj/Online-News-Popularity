03b0366c842990e5024dfb4cb87be46450a374ff
Ankit Sadana <asadana@indiana.edu>
2015-11-24 15:36:46 -0500

    Added resources and plots folder. Changed functionality in RF

--- RandomForest.r
+++ RandomForest.r
@@ -1,8 +1,24 @@
 #install.packages("randomForest")
 library(randomForest)
 setwd("C:/Users/Jaini/Desktop/OnlineNewsPopularity")
-Dummy.data <- read.delim("C:/Users/Jaini/Desktop/OnlineNewsPopularity/Dummy data.txt")
+#setwd("~/workspace/Online-News-Popularity/")
+
+#Dummy.data <- read.delim("C:/Users/Jaini/Desktop/OnlineNewsPopularity/Dummy data.txt")
 #View(Dummy.data)
-rf=randomForest(url~.,data=Dummy.data,mtry=4,ntree=50)
+
+# Reading the database into dat
+dat <- read.table("resources/OnlineNewsPopularity-new-regular-shares.csv", header = TRUE, sep = ",")
+#dat <- read.table("resources/OnlineNewsPopularity-new-binary-shares.csv", header = TRUE, sep = ",")
+
+# If you want to divide shares into say bins of 10
+# dat$group <- as.numeric(cut(dat$shares, 10))
+
+# plotting example between weekday and shares
+plot(dat$weekday, dat$shares, type = "p", 
+     main = "Point plot between weekday and shares",
+     xlab = "Weekday 1-Monday ... 7-Sunday",
+     ylab = "Number of shares")
+
+rf=randomForest(shares~.,data=dat,mtry=4,ntree=20)
 class(rf)
 print(rf$importance)
