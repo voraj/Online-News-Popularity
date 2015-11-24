7b5aa05f986a779fb36dfef95be13ef97500be00
Jaini Vora <voraj@iu.edu>
2015-11-24 18:50:14 -0500

    changes in random forest
    changes in random forest

--- Decisiontree.R
+++ Decisiontree.R
@@ -1,6 +1,34 @@
 setwd("C:/Users/Jaini/Desktop/OnlineNewsPopularity")
-Dummy.data <- read.delim("C:/Users/Jaini/Desktop/OnlineNewsPopularity/Dummy data.txt")
-str(Dummy.data)
+dat <- read.table("resources/OnlineNewsPopularity-new-binary-shares.csv", header = TRUE, sep = ",")
+
+
 library(rpart)
-set.seed(245)
-flags=sample(2,nrow(Dummy.data), replace = TRUE , prob = c(0.7,0.3))
+
+# Spliting dat into training and test
+# Sample size is set to 66% of the rows in dat
+sample_size <- floor(0.66 * nrow(dat))
+# Seed is used here so that you can replicate this train/test dataset
+set.seed(123)
+# train_index <- sample matrix using sample_size from length(nrows)
+train_index <- sample(seq_len(nrow(dat)), size = sample_size)
+
+# Training and test data
+train_dat <- dat[train_index, ]
+test_dat <- dat[-train_index, ]
+
+#Build a decision tree using rpart
+dtree=rpart(shares~.,data=train_dat,control=rpart.control(10))
+str(dtree)
+dtree
+plot(dtree)
+dtree$variable.importance
+
+#Make prediction using dtree
+predictedDT= predict(dtree, test_dat,type=c("matrix"))
+predictedDT
+table(predictedDT,test_dat$shares) #Confusion Matrix
+mean(predictedDT!=test_dat$shares) # 1 ? 
+
+New=read.csv("predict.csv",header=T)
+predict(dtree,New, type=c("matrix"))
+
--- RandomForest.r
+++ RandomForest.r
@@ -3,35 +3,11 @@ library(randomForest)
 setwd("C:/Users/Jaini/Desktop/OnlineNewsPopularity")
 #setwd("~/workspace/Online-News-Popularity/")
 
-#Dummy.data <- read.delim("C:/Users/Jaini/Desktop/OnlineNewsPopularity/Dummy data.txt")
-#View(Dummy.data)
-
 # Reading the database into dat
 dat <- read.table("resources/OnlineNewsPopularity-new-regular-shares.csv", header = TRUE, sep = ",")
-#dat <- read.table("resources/OnlineNewsPopularity-new-binary-shares.csv", header = TRUE, sep = ",")
 
-# Spliting dat into training and test
-# Sample size is set to 66% of the rows in dat
-sample_size <- floor(0.66 * nrow(dat))
-# Seed is used here so that you can replicate this train/test dataset
-set.seed(123)
-# train_index <- sample matrix using sample_size from length(nrows)
-train_index <- sample(seq_len(nrow(dat)), size = sample_size)
 
-# Training and test data
-train_dat <- dat[train_index, ]
-test_dat <- dat[-train_index, ]
-
-# If you want to divide shares into say bins of 10
-# dat$group <- as.numeric(cut(dat$shares, 10))
-
-# plotting example between weekday and shares
-plot(dat$weekday, dat$shares, type = "p", 
-     main = "Point plot between weekday and shares",
-     xlab = "Weekday 1-Monday ... 7-Sunday",
-     ylab = "Number of shares")
-
-rf=randomForest(shares~.,data=dat,mtry=4,ntree=20)
-#plot(rf)
+rf=randomForest(shares~.,data=dat,mtry=4,ntree=20) # Creates the random forest
+plot(rf)  #plots the random forest
 class(rf)
-print(rf$importance)
+print(rf$importance) #prints the importance of various attrbutes in decision making
